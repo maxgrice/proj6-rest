6fdfaff4939f95bbcf003c579dc511859338a654
Max Grice <max@Max-MacBook.local>
2018-03-02 20:05:57 -0800

    made final changes

--- DockerRestAPI/laptop/api.py
+++ DockerRestAPI/laptop/api.py
@@ -1,25 +1,166 @@
 # Laptop Service
+# BEST VERSION
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from pymongo import MongoClient
+import pymongo
 
 # Instantiate the app
 app = Flask(__name__)
 api = Api(app)
 
-class Laptop(Resource):
-    def get(self):
-        return {
-            'Laptops': ['Mac OS', 'Dell', 
-            'Windozzee',
-	    'Yet another laptop!',
-	    'Yet yet another laptop!'
-            ]
-        }
+# ACCESS DATABASE
+client = MongoClient('db', 27017)
+db = client.tododb # gets database
+
+class all(Resource): # use 5001 to access!
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort([('open_times',pymongo.ASCENDING), ('close_times',pymongo.ASCENDING)]).limit(top)
+		li_data = [item for item in _items]
+
+		app.logger.debug("DATA IS")
+		app.logger.debug(li_data)
+		
+		dic = {}
+		otimes = []
+		ctimes = []
+		
+		for item in li_data:
+			otimes.append(item['open_times'])
+			ctimes.append(item['close_times'])
+			
+		app.logger.debug("OPEN IS")
+		app.logger.debug(otimes)
+		app.logger.debug("CLOSE IS")
+		app.logger.debug(ctimes)
+		
+		dic['open_times'] = otimes
+		dic['close_times'] = ctimes
+		
+		return dic
+
+api.add_resource(all, '/listAll')
+
+class all_json(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort([('open_times',pymongo.ASCENDING), ('close_times',pymongo.ASCENDING)]).limit(top)
+		li_data = [item for item in _items]		
+		dic = {}
+		otimes = []
+		ctimes = []		
+		for item in li_data:
+			otimes.append(item['open_times'])
+			ctimes.append(item['close_times'])		
+		dic['open_times'] = otimes
+		dic['close_times'] = ctimes
+		return dic
+
+api.add_resource(all_json, '/listAll/json')
+
+class all_csv(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort([('open_times',pymongo.ASCENDING), ('close_times',pymongo.ASCENDING)]).limit(top)
+		li_data = [item for item in _items]
+		
+		csv_string = ""
+		for item in li_data:
+			csv_string += item['open_times'] + ", "
+			csv_string += item['close_times'] + ", "
+		return csv_string
+
+api.add_resource(all_csv, '/listAll/csv')
+
+class open_json(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort('open_times', pymongo.ASCENDING).limit(top)
+		li_data = [item for item in _items]
+		
+		app.logger.debug("MADE IT HERE")
+		dic = {}
+		otimes = []	
+		for item in li_data:
+			otimes.append(item['open_times'])		
+		dic['open_times'] = otimes
+		return dic
+
+api.add_resource(open_json, '/listOpenOnly/json')
+
+class open_csv(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort('open_times', pymongo.ASCENDING).limit(top)
+		li_data = [item for item in _items]
+		
+		csv_string = ""
+		for item in li_data:
+			csv_string += item['open_times'] + ", "
+		return csv_string
+
+api.add_resource(open_csv, '/listOpenOnly/csv')
+
+class close_json(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort('close_times', pymongo.ASCENDING).limit(top)
+		li_data = [item for item in _items]
+		
+		dic = {}
+		ctimes = []		
+		for item in li_data:
+			ctimes.append(item['close_times'])		
+		dic['close_times'] = ctimes
+		return dic
+
+api.add_resource(close_json, '/listCloseOnly/json')
+
+class close_csv(Resource):
+	def get(self):
+		num = request.args.get('top')
+		if num == None:
+			top = 50
+		else:
+			top = int(num)
+		_items = db.tododb.find().sort('close_times', pymongo.ASCENDING).limit(top)
+		li_data = [item for item in _items]
+		
+		csv_string = ""
+		for item in li_data:
+			csv_string += item['close_times'] + ", "
+		return csv_string
+
+api.add_resource(close_csv, '/listCloseOnly/csv')
 
 # Create routes
 # Another way, without decorators
-api.add_resource(Laptop, '/')
 
 # Run the application
 if __name__ == '__main__':
--- DockerRestAPI/website/index.php
+++ DockerRestAPI/website/index.php
@@ -1,18 +1,54 @@
 <html>
     <head>
-        <title>CIS 322 REST-api demo: Laptop list</title>
+        <title>Brevet Times</title>
     </head>
 
     <body>
-        <h1>List of laptops</h1>
+        <h1>Brevet Time Listings</h1>
         <ul>
             <?php
-            $json = file_get_contents('http://laptop-service/');
-            $obj = json_decode($json);
-	          $laptops = $obj->Laptops;
-            foreach ($laptops as $l) {
-                echo "<li>$l</li>";
-            }
+            $all = file_get_contents('http://laptop-service/listAll');
+            echo "   All Times:\n"
+            foreach ($all as $time) {
+	            echo "<li>$time</li>"
+	        }
+	        
+	        $all_json = file_get_contents('http://laptop-service/listAll/json');
+	        echo "   All Times (JSON):\n"
+            foreach ($all_json as $time) {
+	            echo "<li>$time</li>"
+	        }
+	 
+	        $all_csv = file_get_contents('http://laptop-service/listAll/csv');
+	        echo "   All Times (CSV):\n"
+            foreach ($all_csv as $time) {
+	            echo "<li>$time</li>"
+	        }
+	        
+	        $open_json = file_get_contents('http://laptop-service/listOpenOnly/json');
+	        echo "   Open Times (JSON):\n"
+            foreach ($open_json as $time) {
+	            echo "<li>$time</li>"
+	        }
+	  
+	        $close_json = file_get_contents('http://laptop-service/listCloseOnly/json');
+	        echo "   Close Times (JSON):\n"
+            foreach ($close_json as $time) {
+	            echo "<li>$time</li>"
+	        }
+	
+	        $open_csv = file_get_contents('http://laptop-service/listOpenOnly/csv');
+	        echo "   Open Times (CSV):\n"
+            foreach ($open_csv as $time) {
+	            echo "<li>$time</li>"
+	        }
+	  
+	        $close_csv = file_get_contents('http://laptop-service/listCloseOnly/csv');
+	        echo "   Close Times (CSV):\n"
+            foreach ($open_csv as $time) {
+	            echo "<li>$time</li>"
+	        }
+	
             ?>
         </ul>
     </body>
